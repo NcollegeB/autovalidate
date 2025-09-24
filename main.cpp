9fc6f3a0d84c90100f28b5098681d6fbb48f6a21
NcollegeB <skyryze02@gmail.com>
2025-09-24 11:32:21 -0700

    -

--- main.cpp
+++ main.cpp
@@ -32,8 +32,8 @@ int main()
     getline(cin, input);
     if (input == "nothing")
       return 0;
-    std::transform(input.begin(), input.end(), input.begin(), [](unsigned char c)
-                   { return std::tolower(c); });
+    transform(input.begin(), input.end(), input.begin(), [](unsigned char c)
+              { return std::tolower(c); });
     pick = rand() % VALIDATION.size();
     cout << VALIDATION[pick] << "!\n";
   } while (input != "nothing");
